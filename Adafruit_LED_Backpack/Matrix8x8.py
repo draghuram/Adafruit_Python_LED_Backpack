a3ec47104fe46f6181f0c8935fb7d94218638867
Raghuram Devarakonda <draghuram@gmail.com>
2017-03-11 22:10:37 -0500

    Comment out PIL import.

--- Adafruit_LED_Backpack/Matrix8x8.py
+++ Adafruit_LED_Backpack/Matrix8x8.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 from . import HT16K33
-from PIL import Image
+# from PIL import Image
 import time
 
 
